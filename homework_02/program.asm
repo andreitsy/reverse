21e364918de353e5f44d01ef45df080fe4227c0c
Andrei Tsypilnikov <andrei.tsypilnikov@onetick.com>
2022-01-12 20:35:57 +0300

    fix

--- homework_02/program.asm
+++ homework_02/program.asm
@@ -9,16 +9,42 @@ len dw ?
 .code
 
 main:
-    call read_hex
-    mov bx, ax
-    call read_hex
-    add ax, bx
-    jmp $
+    ;call read_hex
+    ;mov bx, ax
+    ;call read_hex
+    ;add ax, bx
+    mov ax, 0ABDh
     call print_hex
     ret
 
 print_hex:
-   jmp $
+   push cx
+   push dx
+   push bx
+   mov cx, 0
+   mov bx, 10h
+   mov dx, 0
+.more_digit:
+   div bx
+   cmp dx, 9
+   ja .let:
+   add dx, '0'
+   jmp .save
+.let:
+   add dx, 37h
+.save:
+   push dx
+   mov dx, 0
+   inc cx
+   cmp ax, 0
+   jne .more_digit
+   
+   mov ah, 09h
+   int 21h
+   
+   pop bx
+   pop dx
+   pop cx
    ret
     
 read_hex:
@@ -34,7 +60,7 @@ read_hex:
    mov cx, 0
    mov ah, 1
    
-   .rd:
+.rd:
    int 21h
    mov bl, al
    cmp al, 0xD ; press enter?
@@ -46,7 +72,7 @@ read_hex:
    cmp al, 'A'
    jb .wrong
    
-   .digit:
+.digit:
    push bx
    inc cx
    cmp cx, N ; max length?
@@ -61,30 +87,30 @@ read_hex:
    mov bx, 0
    int 80h
    
-   .done:
-   mov len, cx
-   mov ax, 0
-   mov bp, sp
-   add bp, cx
-   add bp, cx
-   .l:
-   sub bp, 2
-   mov bx, 16
-   mul bx
-   mov bl, [bp]
-   cmp bl, '9'
-   ja .letter:
-   sub bx, '0'
-   .letter:
-   sub bx, 'A'
-   add bx, 10
-   add ax, bx       
-   loop .l
+.done:
+    mov len, cx
+    mov ax, 0
+    mov bp, sp
+    add bp, cx
+    add bp, cx
+    .l:
+    sub bp, 2
+    mov bx, 16
+    mul bx
+    mov bl, [bp]
+    cmp bl, '9'
+    ja .letter:
+    sub bx, '0'
+.letter:
+    sub bx, 'A'
+    add bx, 10
+    add ax, bx         
+    loop .l
 
-   mov cx, len  
-   add sp, cx
-   add sp, cx
-   pop dx
-   pop bx
-   pop bp
-   ret
+    mov cx, len  
+    add sp, cx
+    add sp, cx
+    pop dx
+    pop bx
+    pop bp
+    ret
